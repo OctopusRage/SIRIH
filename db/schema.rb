5f164f9aec15f549b1e4d685f3654c4793741d06
octopus <ahyana1995@gmail.com>
2017-05-17 03:12:24 -0400

    fix migratino

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 20170516193503) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.string "token"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
